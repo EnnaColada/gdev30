240896e44bad14ee73052559427bec4155be099c
EnnaColada <enna.veloso@student.ateneo.edu>
2025-03-26 12:29:36 +0800

    Added Camera code
    not working right now

--- GDEVGL/Exercise3.cpp
+++ GDEVGL/Exercise3.cpp
@@ -101,6 +101,30 @@ void render()
                                  glm::vec3(1.0f, 0.0f, 0.0f));
     matrix = glm::scale(matrix, glm::vec3(5.0f, 5.0f, 1.0f));
 
+
+// Camera 
+    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+    //Camera Direction
+    glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+    //Right axis
+    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f); 
+    glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+    //Up axis
+    glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
+    //Look at
+    //glm::mat4 view;
+    // view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f), 
+  	//                    glm::vec3(0.0f, 0.0f, 0.0f), 
+  	// 	                  glm::vec3(0.0f, 1.0f, 0.0f));
+    const float radius = 10.0f;
+    float camX = sin(glfwGetTime()) * radius;
+    float camZ = cos(glfwGetTime()) * radius;
+    glm::mat4 view;
+    view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));  
+
+
+
     glUniformMatrix4fv(glGetUniformLocation(shader, "matrix"),
                        1, GL_FALSE, glm::value_ptr(matrix));
 
